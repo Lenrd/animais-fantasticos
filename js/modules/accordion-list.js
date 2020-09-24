c05e9c2e454b4664620fb2633a9a22c4e4ee7eb4
lenrd <leonardo_aguiar@live.co.kr>
2020-09-23 21:01:21 -0300

    AccordionList passado para classe

--- js/modules/accordion-list.js
+++ js/modules/accordion-list.js
@@ -1,16 +1,27 @@
-export default function initAccordionList() {
-  const faqList = document.querySelectorAll('[data-anime="accordion"] dt');
-  const activeClass = "ativo";
-  faqList[0].classList.add(activeClass);
-  faqList[0].nextElementSibling.classList.add(activeClass);
-
-  function accordionList(listItem) {
-    listItem.classList.toggle(activeClass);
-    listItem.nextElementSibling.classList.toggle(activeClass);
+export default class AccordionList {
+  constructor(list) {
+    this.accordionList = document.querySelectorAll(list);
+    this.activeClass = "ativo";
   }
-  faqList.forEach((item) => {
-    item.addEventListener("click", () => {
-      accordionList(item);
+
+  toggleAccordion(item) {
+    item.classList.toggle(this.activeClass);
+    item.nextElementSibling.classList.toggle(this.activeClass);
+  }
+
+  // adiciona eventos ao accordion
+  addAccordionEvent() {
+    this.accordionList.forEach((item) => {
+      item.addEventListener("click", () => {
+        this.toggleAccordion(item);
+      });
     });
-  });
+  }
+
+  init() {
+    if (this.accordionList.length) {
+      this.toggleAccordion(this.accordionList[0]);
+      this.addAccordionEvent();
+    }
+  }
 }
