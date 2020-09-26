6a62aa36d7a54acfd99ec05e25a52f560eda2af2
lenrd <leonardo_aguiar@live.co.kr>
2020-09-26 15:32:30 -0300

    ScrollAnimatio refeito, para melhor performance

--- js/script.js
+++ js/script.js
@@ -8,7 +8,7 @@ import initDropdownMenu from "./modules/dropdown-menu.js";
 import initMobileMenu from "./modules/menu-mobile.js";
 import initFuncionamento from "./modules/funcionamento.js";
 import fetchAnimais from "./modules/fetch-animais.js";
-import fetchBitcoin from "./modules/fetch-bitcoin.js";
+// import fetchBitcoin from "./modules/fetch-bitcoin.js";
 
 const scrollSuave = new ScrollSuave('[data-menu="suave"] a[href^="@"]');
 scrollSuave.init();
@@ -39,4 +39,4 @@ initDropdownMenu();
 initMobileMenu();
 initFuncionamento();
 fetchAnimais("./animaisApi.json", ".numeros-grid");
-fetchBitcoin("https://blockchain.info/ticker", ".btc-preco");
+// fetchBitcoin("https://blockchain.info/ticker", ".btc-preco");
